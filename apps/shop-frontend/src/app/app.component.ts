21605c933daa8fdb9a677f993381a8e7cebe6770
9hyd5xyxxb <9hyd5xyxxb@mail.ru>
2023-06-05 09:55:11 +0500

    init

---
+++ apps/shop-frontend/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "telegram-shop-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
+})
+export class AppComponent {
+  title = "shop-frontend";
+}
