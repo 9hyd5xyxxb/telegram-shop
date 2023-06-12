6405cc2ee5d16541d5704d77ae469ae7fec6b7f1
9hyd5xyxxb <9hyd5xyxxb@mail.ru>
2023-06-12 21:55:38 +0500

    prepare to interact tg with app

--- apps/shop-frontend/src/app/app.module.ts
+++ apps/shop-frontend/src/app/app.module.ts
@@ -7,14 +7,27 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UiModule } from "@telegram-shop/ui";
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { AddressFormComponent } from './address-form/address-form.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCardModule } from '@angular/material/card';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, DashboardComponent, NavigationComponent],
+  declarations: [AppComponent, DashboardComponent, NavigationComponent, AddressFormComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: "enabledBlocking" }),
     BrowserAnimationsModule,
     UiModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatCardModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- apps/shop-frontend/src/app/app.routes.ts
+++ apps/shop-frontend/src/app/app.routes.ts
@@ -1,14 +1,19 @@
 import { Route } from "@angular/router";
 import { DashboardComponent } from "./dashboard/dashboard.component";
+import { AddressFormComponent } from "./address-form/address-form.component";
 
 export const appRoutes: Route[] = [
     {
         path: '',
-        redirectTo: 'dashboard',
+        redirectTo: 'address-form',
         pathMatch: 'full',
     },
     {
         path: 'dashboard',
         component: DashboardComponent,
-    }
+    },
+    {
+        path: 'address-form',
+        component: AddressFormComponent,
+    },
 ];
